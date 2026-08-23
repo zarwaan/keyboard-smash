fd978d5ec68b449bc238160c9945c8d7e8bbda57
zarwaan <zarwaanshroff@gmail.com>
2026-08-23 15:16:59 +0530

    [fix] Score not displaying on infinite mode

--- src/components/GameDisplay/ScoreDisplay/Score.tsx
+++ src/components/GameDisplay/ScoreDisplay/Score.tsx
@@ -12,14 +12,17 @@ export default function Score({}) {
     return (
         <div className=" w-fit text-(--text-color) theme-transition text-4xl font-bold flex flex-col gap-6">
             {
-                playMode!=="infinite" && gameState!=='stopped' &&
+                gameState!=='stopped' &&
                 <>
                     <div className="flex flex-row gap-10">
                         <span>Targets hit : {score.targetsHit}</span>
                         <span>Targets missed : {score.targetsMissed}</span>
                         <span>Bombs hit : {score.bombsHit}</span>
                     </div>
-                    <Lives key={'new'}/>
+                    {
+                        playMode!=="infinite" && 
+                        <Lives key={'new'}/>
+                    }
                 </>
             }
         </div>
