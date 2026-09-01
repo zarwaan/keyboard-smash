8f97d93f9fdd39137ae5438906b4c073f3bf7459
zarwaan <zarwaanshroff@gmail.com>
2026-09-01 18:58:23 +0530

    Added new instruction box

--- src/components/MainContent/ContentLayout.tsx
+++ src/components/MainContent/ContentLayout.tsx
@@ -4,7 +4,8 @@ import Header from "./Header";
 import MainBody from "./MainBody";
 import GameControls from "../GameDisplay/ControlDisplay/GameControls";
 import Score from "../GameDisplay/ScoreDisplay/Score";
-import InstructionBox from "../Instructions/InstructionBox";
+// import InstructionBox from "../Instructions/InstructionBox";
+import InstructionBox2 from "../Instructions/2/InstructionBox2";
 
 export default function ContentLayout() {
     return (
@@ -17,7 +18,7 @@ export default function ContentLayout() {
                 <Score />
             </MainBody>
             <SettingsBox />
-            <InstructionBox />
+            <InstructionBox2 />
         </div>
     )
 }
